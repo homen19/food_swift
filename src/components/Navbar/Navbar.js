471134582ee9884daad67aa4f61b9786f0b5dd38
Homen Nath <homennath7638@gmail.com>
2023-09-23 14:12:59 +0530

    Navbar created

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css';
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className='nav'>
+        <h2>This is the Navbar compoent</h2>
+
+      </div>
+    </>
+
+  )
+}
