133826324073147d17ed207885ebc370a2f9dc67
Homen Nath <homennath7638@gmail.com>
2023-09-24 16:07:42 +0530

    changed the changes

--- src/components/ItemCategories/ItemCaterory.js
+++ src/components/ItemCategories/ItemCaterory.js
@@ -39,18 +39,7 @@ export const ItemCategory = () => {
             </div>
             <p>Biriyani</p>
           </div>
-          <div className="item-cat-card">
-            <div className="items-cat-type">
-              <img src={biriyaniType} alt="" /> 
-            </div>
-            <p>Biriyani</p>
-          </div>
-          <div className="item-cat-card">
-            <div className="items-cat-type">
-              <img src={biriyaniType} alt="" /> 
-            </div>
-            <p>Biriyani</p>
-          </div>
+          
           <div className="item-cat-card">
             <div className="items-cat-type">
               <img src={biriyaniType} alt="" /> 
