8785efffcb37280efa4c93c129576dfe5a8f0121
Homen Nath <65844503+homen19@users.noreply.github.com>
2023-09-24 15:33:48 +0530

    Homen1 (#8)
    * image category style added
    
    * item category done

---
+++ src/components/ItemCategories/ItemCaterory.js
@@ -0,0 +1,65 @@
+
+import React from 'react'
+import './ItemCategory.css';
+import biriyaniType from "../../assets/Images/biryaniType.png";
+export const ItemCategory = () => {
+  return (
+    <>
+      <div className="item-category">
+        <h2>Image Category</h2>
+        <div className="items-cat">
+          <div className="item-cat-card">
+            <div className="items-cat-type">
+              <img src={biriyaniType} alt="" /> 
+            </div>
+            <p>Biriyani</p>
+          </div>
+
+          <div className="item-cat-card">
+            <div className="items-cat-type">
+              <img src={biriyaniType} alt="" /> 
+            </div>
+            <p>Biriyani</p>
+          </div>
+          <div className="item-cat-card">
+            <div className="items-cat-type">
+              <img src={biriyaniType} alt="" /> 
+            </div>
+            <p>Biriyani</p>
+          </div>
+          <div className="item-cat-card">
+            <div className="items-cat-type">
+              <img src={biriyaniType} alt="" /> 
+            </div>
+            <p>Biriyani</p>
+          </div>
+          <div className="item-cat-card">
+            <div className="items-cat-type">
+              <img src={biriyaniType} alt="" /> 
+            </div>
+            <p>Biriyani</p>
+          </div>
+          <div className="item-cat-card">
+            <div className="items-cat-type">
+              <img src={biriyaniType} alt="" /> 
+            </div>
+            <p>Biriyani</p>
+          </div>
+          <div className="item-cat-card">
+            <div className="items-cat-type">
+              <img src={biriyaniType} alt="" /> 
+            </div>
+            <p>Biriyani</p>
+          </div>
+          <div className="item-cat-card">
+            <div className="items-cat-type">
+              <img src={biriyaniType} alt="" /> 
+            </div>
+            <p>Biriyani</p>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
