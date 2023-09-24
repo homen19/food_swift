467d4cfc8fff7e9c4a5d099d72bb784cfa2c2507
Homen Nath <homennath7638@gmail.com>
2023-09-24 13:54:49 +0530

    Item category added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from '../../components/Navbar/Navbar'
 import { ImageSlider } from '../../components/ImageSlider/ImageSlider'
+import { ImageCategory } from '../../components/ItemCategories/ImageCategory'
 
 export const Home = () => {
   return (
     <>
         <Navbar />
         <ImageSlider />
+        <ImageCategory />
     </>
   )
 }
