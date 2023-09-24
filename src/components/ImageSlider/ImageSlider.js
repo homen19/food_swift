6d79660c508d410ee40730284fe341add05915d0
Sweta Kumari Mishra <mishrasweta1001@gmail.com>
2023-09-24 16:06:24 +0530

    images and text added to image-slider

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './ImageSlider.css';
+import foodimg from './Images/foodimg.png';
 export const ImageSlider = () => {
   return (
     <>
-        <div className="image-slider">
-            <h2>Image Slider</h2>
-            <div className ="image">
-              <h1>Images</h1>
-            </div>
+      <div className="image-slider">
+        <div className='image-slider-container'>
+          <div className="text">
+            <h3>50% discount on all products</h3>
+            <h2>Enjoy Your <span className='s1'>Special</span></h2>
+            <h2>Delicious meal</h2>
+          </div>
+          <div className='image'>
+            <img src= {foodimg}></img>
+          </div>
         </div>
-        
+      </div>
+
     </>
   )
 }
